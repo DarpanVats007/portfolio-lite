c4492222276109f066e9c1e3f47a5cf6a1926f4a
Darpan Vats <dva@protechnology.de>
2024-05-22 20:47:10 +0200

    - package changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { checker } from "vite-plugin-checker";
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+  //specify the jsxFactory and jsxFragment properties to tell Vite.js how to process JSX code.
+  esbuild: {
+    jsxFactory: "React.createElement",
+    jsxFragment: "React.Fragment",
+  },
+  css: {
+    modules: {
+      localsConvention: "camelCaseOnly",
+    },
+  },
+  root: "./",
+  build: {
+    outDir: "build",
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "index.html"),
+      },
+    },
+  },
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+    }),
+    viteCompression({
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
+  base: "/job-posting/",
+});
